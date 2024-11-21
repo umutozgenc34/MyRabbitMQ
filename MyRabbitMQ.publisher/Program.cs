a89be3a54b81ef3b0d55ccfd1b3964beba3d2cc5
umutozgenc34 <umutozgenc34@hotmail.com>
2024-11-21 20:49:05 +0300

    connection

--- MyRabbitMQ.publisher/Program.cs
+++ MyRabbitMQ.publisher/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://lifgdraw:***************************@chimpanzee.rmq.cloudamqp.com/lifgdraw");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+channel.QueueDeclare("first-queue", true, false, false);
+
+string message = "Hello World";
+var messageBody = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(string.Empty, "first-queue",null,messageBody);
+
+Console.WriteLine("Mesaj gönderildi.");
+Console.ReadLine();
--- MyRabbitMQ.subscriber/Program.cs
+++ MyRabbitMQ.subscriber/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://lifgdraw:*************************@chimpanzee.rmq.cloudamqp.com/lifgdraw");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+//channel.QueueDeclare("first-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("first-queue", false, consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine($"Gelen mesaj : {message} ");
+};
+
+Console.ReadLine();
